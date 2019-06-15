bcbdf50a1588d9d3117509ce6b225d607c66dc8f
joshlewis-py <joshua-l-lewis@outlook.com>
2019-06-15 14:25:07 -0400

    Added input fields, script will not need to hold uername and passwords

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import sys
 import os
+import getpass
 from github import Github
 
 path = "/home/osboxes/Documents/Projects/"
 
 def create():
-    folderName = str(sys.argv[1])
-    os.makedirs(path + str(sys.argv[1]))
-    print("Succesfully created repository {}".format(sys.argv[1]))
 
+	if len(sys.argv) > 1:
+		x = sys.argv[1]
+		print("the project folder and repository will be called ", x)
+	else:
+		x = input("Project tile: ")
+
+	
+	username = input("Enter Github username: ")
+	password = getpass.getpass("Enter Github password: ")
+	repo = user.create_repo(x)
+	folderName = str(x)
+	os.makedirs(path + str(x))
+	user = Github(username, password).get_user()
+	
+	print("Succesfully created repository ", x)
+
+	
 if __name__ == "__main__":
 	create()

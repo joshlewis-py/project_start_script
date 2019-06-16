88985663eeb0bbce504577d01ef71b4edfc3b191
joshlewis-py <joshua-l-lewis@outlook.com>
2019-06-16 11:25:15 -0400

    added some code and fixed errors.

--- main.py
+++ main.py
@@ -7,19 +7,23 @@ path = "/home/osboxes/Documents/Projects/"
 
 def create():
 
+	# If no parameters passed ask for a project name
 	if len(sys.argv) > 1:
 		x = sys.argv[1]
 		print("the project folder and repository will be called ", x)
 	else:
 		x = input("Project tile: ")
-
 	
+	# I want to somehow verify username and passord
 	username = input("Enter Github username: ")
 	password = getpass.getpass("Enter Github password: ")
-	repo = user.create_repo(x)
-	folderName = str(x)
+
+	# Make holder named parameter	
 	os.makedirs(path + str(x))
+	
+	# Get Github account and create repo after the parameter
 	user = Github(username, password).get_user()
+	repo = user.create_repo(x)
 	
 	print("Succesfully created repository ", x)
 

632d6b3096b1b6fe8526135498dadc9d985958f8
joshlewis-py <joshua-l-lewis@outlook.com>
2019-06-18 15:52:45 -0400

    currecly an infinite loop, I know why just need time

--- main.py
+++ main.py
@@ -4,45 +4,47 @@ import getpass
 from github import Github
 
 
+x = "defult"
+path = ("/home/osboxes/Documents/Projects/"+str(x))
+
 
 def verification():
 
+	# check if x already exsists
+
 	if len(sys.argv) > 1:
 		x = sys.argv[1]
-
-		print("the project folder and repository will be called ", x)
+		if not os.path.exists(path):
+			print("File already exsists try again")
+			verification()
+		else:
+			print("the project folder and repository will be called ", x)
 	else:
-		 x = input("Project tile: ")
+		x = input("Project tile: ")
+
 
 	# need to check if file already exsists
-
-
 	username = input("Enter Github username: ")
 	password = getpass.getpass("Enter Github password: ")
-
 	create(x, username, password)
+
 
 
 def create(x, username, password):
 
-	# I want to somehow verify username and passord	
-	path = ("/home/osboxes/Documents/Projects/"+str(x))
+	# I want to somehow verify username and passord
 
-	# Make folder named parameter, os.makedirs(path + str(x))	
+
+	# Make folder named parameter, os.makedirs(path + str(x))
 	os.makedirs(path)
-	
+
 	# Get Github account and create repo after the parameter
 	user = Github(username, password).get_user()
 	repo = user.create_repo(x)
-	
+
 	print("Succesfully created repository ", x)
 
 
 
-
-
-
-	
 if __name__ == "__main__":
 	verification()
-

468e918ca0f82d199c28172975348f13ee430b47
joshlewis-py <joshua-l-lewis@outlook.com>
2019-06-23 14:38:41 -0400

    need to fix github()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import os.path
+import os
 import getpass
 from github import Github
 
@@ -8,7 +8,7 @@ path = ("/home/osboxes/Documents/Projects/")
 
 
 def verification():
-	# check if file already exsists and if there was paramenter in the connand
+	# Check if file already exsists and if there is an arguement in the connand.
 	while True:
 		if len(sys.argv) > 1:
 			x = str(sys.argv[1])
@@ -18,36 +18,48 @@ def verification():
 				x = input("Project title: ")
 
 			else:
-				print("the project is called ", x)
 				github(x)
 				break
 
 		else:
 			x = input("project title: ")
-
 			if os.path.exists(path+str(x)):
 				print("file already exists, try again")
-				x = input("Project title: ")
 
 			else:
-				print("the project is called ", x)
 				github(x)
 				break
 
 
 def github(x):
+	# Github Login, with verification.
 	print("Enter your login details to create", x)
-	# need to check if details are correct
-	username = input("Enter Github username: ")
-	password = getpass.getpass("Enter Github password: ")
+
+	while True:
+
+		try:
+			username = input("Enter Github username: ")
+			password = getpass.getpass("Enter Github password: ")
+			break
+
+		except:
+			print("Wrong login details, try again!")
+
+
 	create(x, username, password)
 
 
+
+
+
+
+
+
 def create(x, username, password):
-	# create folder and readme file on the system
+	# Create folder and README.md file on the local system.
 	os.makedirs(path+(str(x)))
 
-	# Get Github account and create repo named after the project
+	# Get Github account and create repo named after the project.
 	user = Github(username, password).get_user()
 	repo = user.create_repo(x)
 	print("Succesfully created repository ", x)
